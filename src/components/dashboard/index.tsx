6144f5d0c701cb2dad9843d3a7c9cc3b42d7eb1e
tmz81 <tmz_81@hotmail.com>
2022-07-14 22:51:49 -0300

    Create dashboard component

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../summary";
+import { Container } from "./style";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}
