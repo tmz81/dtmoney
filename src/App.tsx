df19f481e97577880167f7567773bbe6d5f728b2
tmz81 <tmz_81@hotmail.com>
2022-07-14 21:27:37 -0300

    Create summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/header"
+import { Summary } from "./components/summary"
 import { Global } from "./styles/global"
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Summary></Summary>
       <Global />
     </>
   )
---
+++ src/components/summary/index.tsx
@@ -0,0 +1,33 @@
+import { Container, } from './style';
+import iconInput from '../../assets/entradas.svg';
+import iconOutput from '../../assets/saidas.svg';
+import iconTotal from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Input</p>
+          <img src={iconInput} alt="Input" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Output</p>
+          <img src={iconOutput} alt="Output" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Total</p>
+          <img src={iconTotal} alt="Total" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      
+    </Container>
+  )
+}
