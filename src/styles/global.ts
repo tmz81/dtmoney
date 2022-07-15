985df2844d4e563a9515293c382a11ab3fdfe777
tmz81 <tmz_81@hotmail.com>
2022-07-14 21:27:05 -0300

    media-query

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,9 +19,28 @@ export const Global = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.25%;
+    }
+  }
+
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: Poppins, sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
