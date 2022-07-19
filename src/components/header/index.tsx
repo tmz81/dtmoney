b203751a9b2c9d14507097c78d00462b09a4d422
tmz81 <tmz_81@hotmail.com>
2022-07-19 20:56:56 -0300

    config react-modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.svg'
 import { Container, Content } from "./style";
+import Modal from "react-modal";
 
 export function Header() {
+  const [ isOpenModal, setIsOpenModal ] = useState(false);
+
+  function openModal() {
+    setIsOpenModal(true)
+  }
+
+  function closeModal() {
+    setIsOpenModal(false)
+  }
+  
   return(
     <Container>
       <Content>
         <img src={logo} alt="dt money" />
-        <button>
+        <button type="button" onClick={openModal}>
           New Transition
         </button>
+
+        <Modal 
+          isOpen={isOpenModal}
+          onRequestClose={closeModal}
+          >
+          <h1>New Transactions Modal</h1>
+        </Modal>
       </Content>
     </Container>
   )
