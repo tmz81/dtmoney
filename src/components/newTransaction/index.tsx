727d946fbf6a26434a81c81f70d3e57e9cdfbfe8
tmz81 <tmz_81@hotmail.com>
2022-07-25 10:12:34 -0300

    struct form

--- src/components/newTransaction/index.tsx
+++ src/components/newTransaction/index.tsx
@@ -8,13 +8,22 @@ interface NewTransactionProps {
 
 export function NewTransaction({ isOpen, onRequestClose } : NewTransactionProps){
   return (
-    <Container>
-      <Modal 
-          isOpen={isOpen}
-          onRequestClose={onRequestClose}
-          >
-          <h1>New Transactions Modal</h1>
-        </Modal>
-    </Container>
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      >
+      <Container>
+          <h2>Cadastrar Nova Transação</h2>
+          <input placeholder="Nome"/>
+          <input 
+            type="number"
+            placeholder="Preço"
+          />
+          <input placeholder="Categoria"/>
+          <button type="submit">
+              Cadastrar
+          </button>
+        </Container>
+    </Modal>
   )
 }
