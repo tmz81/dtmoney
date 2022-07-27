8257e9e350d07bb998f90222542f689270472f06
tmz81 <tmz_81@hotmail.com>
2022-07-27 16:20:23 -0300

    calculator summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,7 +8,29 @@ import { TransactionsContext } from '../../context/TransactionsContext';
 export function Summary() {
   const {transactions} = useContext(TransactionsContext);
   
-  console.log(transactions);
+  // const totalDeposits = transactions.reduce((acc, transaction) => {
+  //   if(transaction.type === 'deposit') {
+  //     return acc + transaction.amount;
+  //   }
+
+  //   return acc
+  // }, 0)
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  })
 
   return (
     <Container>
@@ -17,21 +39,32 @@ export function Summary() {
           <p>Input</p>
           <img src={iconInput} alt="Input" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Output</p>
           <img src={iconOutput} alt="Output" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>-{new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={iconTotal} alt="Total" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
